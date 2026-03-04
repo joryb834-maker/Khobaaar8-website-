b38ba2385a7538f4b513ff36a679e5ff2cdc5750
joryb834-maker <joryb834@gmail.com>
2026-03-04 20:20:22 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,17 @@
+function showMessage(){
+  alert("أهلا بكم في قسم التقنية بمدينة الخبر🚀");
+}
+
+function foundingMessage() {
+  alert("🇸🇦يوم التأسيس يمثل العمق التاريخي والحضاري للمملكة العربية السعودية");
+}
+
+function toggleMenu() {
+  var menu = document.getElementById("navMenu");
+  
+  if (menu.style.display === "block") {
+  menu.style.display = "none" ;  
+  } else{
+  menu.style.display = "block" ;  
+  }
+  }
